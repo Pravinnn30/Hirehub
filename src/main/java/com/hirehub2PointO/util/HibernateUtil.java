9ca9c7baefaeb7a4d0353c22a1e79f5cf851d91d
Pravin <pravinmaithil8168@gmail.com>
2026-01-06 15:49:09 +0530

    Clean up HibernateUtil by removing commented code
    Removed commented-out code for HibernateUtil class.

--- src/main/java/com/hirehub2PointO/util/HibernateUtil.java
+++ src/main/java/com/hirehub2PointO/util/HibernateUtil.java
@@ -5,23 +5,6 @@ import org.hibernate.cfg.Configuration;
 
 import java.util.Properties;
 
-//public class HibernateUtil {
-//    private static SessionFactory sessionFactory;
-//
-//    static {
-//        try {
-//            if (sessionFactory == null) {
-//                sessionFactory = new Configuration().configure("Hibernate.cfg.xml").buildSessionFactory();
-//            }
-//        } catch (Throwable e) {
-//            throw new ExceptionInInitializerError(e);
-//        }
-//    }
-//
-//    public static SessionFactory getSessionFactory() {
-//        return sessionFactory;
-//    }
-//}
 public class HibernateUtil {
     private static SessionFactory sessionFactory;
     private static Properties props = ConfigUtil.getProperties();
